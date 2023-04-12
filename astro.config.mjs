418a1f1b8a6ae7530d082a3ee5a2be42f5e10455
ma_shi_chen <1934202608@qq.com>
2023-04-12 23:53:56 +0800

    update markdownPostLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import addClasses from "rehype-add-classes";
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
-  integrations: [react(), tailwind()]
+    // Enable React to support React JSX components.
+    integrations: [react(), tailwind()],
+    markdown: {
+        rehypePlugins: [
+            [
+                addClasses,
+                {
+                    h1: "text-4xl font-bold mb-6",
+                    h2: "text-2xl font-bold mb-6",
+                    h3: "text-xl font-bold",
+                    h4: "text-lg font-bold",
+                    h5: "font-bold",
+                    img: "border border-slate-300 dark:border-zinc-700 rounded-xl mb-6",
+                    p: "mb-6",
+                    a: "underline underline-offset-2 hover:text-orange-500 decoration-orange-500",
+                    ul: "list-disc",
+                },
+            ],
+        ],
+    },
 });
